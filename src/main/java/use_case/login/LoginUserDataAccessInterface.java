5a2d15213dd77439b71903d7f1894c4742724428
AhmadIbrahimWaqas <157913756+AhmadIbrahimWaqas@users.noreply.github.com>
2024-10-21 16:03:19 -0400

    Tak 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -26,6 +26,18 @@ public interface LoginUserDataAccessInterface {
      * @return the user with the given username
      */
     User get(String username);
+    /**
+     * Sets the current user with the provided name.
+     *
+     * @param name the name of the user to set as the current user.
+     */
 
     void setCurrentUser(String name);
+    /**
+     * Retrieves the current user's name.
+     *
+     * @return the name of the current user.
+     */
+
+    String getCurrentUser();
 }
